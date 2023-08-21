e6973d301c7f62906be6974164a33483f02a4db5
Aakarsh Vinay <102870828+akarxxx1030@users.noreply.github.com>
2023-08-21 23:41:41 +0530

    signedint.c

---
+++ signedint.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	long long int max = (long long int) (pow(2,63) -1);
+	long long int min = (long long int) (pow(2,63) *(-1));
+	printf("lowest number represented by signed 64-bit integer is %lld\n",min);
+	printf("highest number represented by signed 64-bit integer is %lld\n",max);
+	return 0;
+}
